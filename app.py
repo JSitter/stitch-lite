4cbf31543d8e09dbbd9029bcf939d2323f8c60fa
Justin Sitter <justin@jaytria.com>
2019-03-28 17:45:41 -0700

    add sqlalchemy orm with basic schema

--- app.py
+++ app.py
@@ -1,9 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template
+from sqlalchemy import create_engine, Column, Integer, String, Unicode
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+import os
+
+Base = declarative_base()
+
+class Product(Base):
+  __tablename__ = 'product'
+
+  id = Column('id', Integer, primary_key=True)
+  product_name = Column('product_name', Unicode, nullable=False)
+  description = Column('description', Unicode, nullable=False)
+  sku = Column('sku', String, unique=True)
+  qty = Column('qty', Integer, nullable=False)
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+print('hello')
+product = Product()
+product.product_name = "Earringsa"
+product.description = "These are wonderful earrings with high quality metals"
+product.sku = '10003003'
+product.qty = 1000
+
+session.add(product)
+session.commit()
+session.close()
+
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return 'Welcome Beings!'
+  return os.environ.get('SHOPIFY_API_KEY')
+
+@app.route('/api/sync', methods=['POST'])
+def sync():
+  return os.environ.get('SHOPIFY_API_KEY')
 
 if __name__ == '__main__':
   app.run()
